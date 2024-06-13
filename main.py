3d5b274530ddd7b22149c50f9bac54de86e37799
ibheelz <ibheelz@icloud.com>
2024-06-13 13:26:28 +0100

    Created circle container

---
+++ main.py
@@ -0,0 +1,21 @@
+import turtle
+
+### Create Turtle Screen ###
+
+screen = turtle.Screen()
+screen.title("C H A O S    B A L L")
+screen.bgcolor("black")
+
+### Create Circle Container ###
+
+o = turtle.Turtle()
+o.hideturtle()
+o.speed(100)
+o.color("white")
+o.pensize(3)
+o.penup()
+o.goto(0, -300)
+o.pendown()
+o.circle(300)
+
+screen.mainloop()
