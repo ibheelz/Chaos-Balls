8e0b2b08587762a597ac931dd3a68653ffbbb523
ibheelz <ibheelz@icloud.com>
2024-07-03 20:55:07 +0100

    Ball is hitting border

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import turtle
 import random
+import math
 
-colors = ["red", "green", "blue", "white", "pink", "purple", "yellow", "grey", "brown", "cyan", "magneta"]
+colors = ["red", "green", "blue", "pink", "purple", "yellow", "grey", "brown", "cyan"]
 
 ### Create Turtle Screen ###
 
@@ -11,32 +12,43 @@ screen.bgcolor("black")
 
 ### Create Circle Container ###
 
-o = turtle.Turtle()
-o.hideturtle()
-o.speed(100)
-o.color("white")
-o.pensize(3)
-o.penup()
-o.goto(0, -300)
-o.pendown()
-o.circle(300)
+container = turtle.Turtle()
+container.hideturtle()
+container.speed(100)
+container.color("white")
+container.pensize(3)
+container.penup()
+container.goto(0, -300)
+container.pendown()
+container.circle(300)
 
 ### Create a new ball ###
 
 balls = []
 
-ball = turtle.Turtle()
-ball.penup()
-ball.shape("circle")
-ball.shapesize(0.5, 0.5)
-ball.color(random.choice(colors))
-ball.setheading(random.randint(0, 360))
-balls.append(ball)
+def create_ball():
+    ball = turtle.Turtle()
+    ball.penup()
+    ball.shape("circle")
+    ball.shapesize(0.5, 0.5)
+    ball.color(random.choice(colors))
+    ball.setheading(random.randint(0, 360))
+    balls.append(ball)
+
+create_ball()
 
 ### Move ball ###
+
 while True:
     for ball in balls:
         ball.forward(1)
+        x, y = ball.position()
+        distance_from_center = math.sqrt(x**2 + y**2)
+        if distance_from_center >= 295:
+            create_ball()
+            ball.setheading(-(random.randint(0, 360)))
+            ball.forward(1)
+
 
 
 
